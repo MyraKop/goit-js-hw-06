ed497b577f77d96490522de8422adb53d7d932e6
MyraKop <136658244+MyraKop@users.noreply.github.com>
2023-11-02 15:24:25 -0400

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,26 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = 
+  document.querySelector('#ingredients')
+
+// const firstItem = document.createElement('li');
+// firstItem.textContent = 'Potatoes';
+// console.log(firstItem)
+
+// const secondItem = document.createElement('li');
+// secondItem.textContent = 'Mushrooms';
+// console.log(secondItem)
+
+const items = ingredients.map(ingredient =>
+{
+  const element = document.createElement('li')
+  console.log(element)
+  element.classList.add('item')
+  element.textContent = ingredient
+  return element;
+})
+  
+ingredientsList.append(...items)
+
